e9e04cc239f5efe7a6fce54a615775c1a82d052c
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-01-08 18:36:56 -0800

    handles empty strings

--- src/main.c
+++ src/main.c
@@ -6,28 +6,44 @@
 #include <sys/wait.h>
 #include "prompt.h"
 
+// execut the command denoted in cmd
+void exec(char * cmd) {
+  char *argv[1] = { "" };
+  int retval;
+  retval = execvp(cmd, argv);
+  if(retval == -1) {
+    perror(cmd);
+  }
+}
+
 int start() {
   int status;
   pid_t pid;
   char *buf, *prompt_str;
 
-  for(pid = 1; pid != 0;) {
+  for(pid = 1; pid != 0; free(buf)) {
     prompt_str = get_prompt();
     buf = readline(prompt_str);
     free(prompt_str);
 
+    // if  is received exit
+    if(buf == NULL) {
+      printf("\nGoodbye!");
+      exit(0);
+    }
+    // empty string don't do anything
+    else if(strcmp(buf, "") == 0) {
+      continue;
+    }
+
     pid = fork();
 
     if(pid == 0) {
-      char *argv[1] = { "" };
-      execvp(buf, argv);
-      // TODO spit error
+      exec(buf);
     }
     else {
-      waitpid(pid, &status, 0);
+      waitpid(pid, &status, 0); // wait so output comes before prompt
     }
-
-    free(buf);
   }
   return 0;
 }
