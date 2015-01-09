d8261b96c5f7b9be94b5496a652a0174b9120539
Dylan Kendal <dkendal@freerunningtechnologies.com>
2015-01-08 16:05:13 -0800

    run binaries from shell

--- src/main.c
+++ src/main.c
@@ -1,23 +1,38 @@
 #include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include "prompt.h"
 
-
 int start() {
+  int status;
+  pid_t pid;
   char *buf, *prompt_str;
-  prompt_str = get_prompt();
 
-  buf = readline(prompt_str);
+  for(pid = 1; pid != 0;) {
+    prompt_str = get_prompt();
+    buf = readline(prompt_str);
+    free(prompt_str);
 
-  free(prompt_str);
-  free(buf);
+    pid = fork();
+
+    if(pid == 0) {
+      char *argv[1] = { "" };
+      execvp(buf, argv);
+      // TODO spit error
+    }
+    else {
+      waitpid(pid, &status, 0);
+    }
+
+    free(buf);
+  }
   return 0;
 }
 
 int main() {
-  while(1) {
-    start();
-  }
+  start();
   return 0;
 }
